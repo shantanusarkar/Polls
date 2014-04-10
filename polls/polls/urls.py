3308a54d7efd055cbf00ae4ef2e15b3ba86193c8
Shantanu Sarkar <shantanusarkar.me@gmail.com>
2014-04-10 12:16:06 +0530

    written more views

--- polls/polls/urls.py
+++ polls/polls/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-
+from polls_app import views
 from django.contrib import admin
 admin.autodiscover()
 
@@ -7,7 +7,7 @@ urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'polls_app.views.home'),
     # url(r'^blog/', include('blog.urls')),
-
     url(r'^polls_app/', include('polls_app.urls')),
     url(r'^admin/', include(admin.site.urls)),
+
 )
--- polls/polls_app/views.py
+++ polls/polls_app/views.py
@@ -9,3 +9,11 @@ def index(request):
 def home(request):
     return render_to_response('index.html')
 
+def detail(request, poll_id):
+    return HttpResponse("You're looking at poll %s." % poll_id)
+
+def results(request, poll_id):
+    return HttpResponse("You're looking at the results of poll %s." % poll_id)
+
+def vote(request, poll_id):
+    return HttpResponse("You're voting on poll %s." % poll_id)
