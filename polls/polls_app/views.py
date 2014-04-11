86739ee59e1b6b73b8036217f12ed153475ae904
Shantanu Sarkar <shantanusarkar.me@gmail.com>
2014-04-11 22:49:57 +0530

    writing more views

--- polls/polls_app/views.py
+++ polls/polls_app/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
+from polls_app.models import Poll
 # Create your views here.
 
 def index(request):
     return HttpResponse("Hello, world. You're at the poll index.")
 
 def home(request):
-    return render_to_response('index.html')
+    latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
+    output = ','.join([p.question for p in latest_poll_list ])
+    return HttpResponse(output)
 
 def detail(request, poll_id):
     return HttpResponse("You're looking at poll %s." % poll_id)
